99b96f9be34caf3188614b8e8a54af80ebfee168
Todd Blackmon <todd.blackmon@gmail.com>
2021-06-07 23:28:50 -0500

    forth words calling forth words is working. Also added return stack handling for do/loop structure

--- cforth.c
+++ cforth.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 
 // Need this to determine which are atomic vs, non-atomic functions
 // This isn't the normal way to do it, but will match how the 8051 works.
@@ -12,14 +14,40 @@
 
 // This is implementing a simple fixed forth word to perform the following:
 // : fib  ( n -- )  1 1 rot 0 do dup rot dup . + loop drop drop ; 
-// : idea#1         10 fib ;
+// : pgm_1         10 fib ;
 
 typedef    void*(*fword)(void);
 
 fword* i_ptr = NULL;
 
+#define get_shift()    3*(tos - BASE_OF_STACK)
+
 #define FORTH_WORD(wp)       (fword)((uint32_t)(wp) + 1)
 #define FORTH_LIT(x)         (fword)(x)
+
+#define PRINT_ADDR
+#ifdef PRINT_ADDR
+#define print_fn(p)  printf("%p: %*s%s\n", p, get_shift(), "", __func__)
+#define print_fn_msg(p,m)  printf("%p: %*s%s %s\n", p, get_shift(), "", __func__, m)
+#else
+#define print_fn(p)  printf("%*s%s\n", get_shift(), "", __func__)
+#define print_fn_msg(p,m)  printf("%*s%s %s\n", get_shift(), "", __func__, m)
+#endif
+
+#define BASE_OF_STACK   10
+uintptr_t return_stack[1000];
+unsigned int tos;
+
+inline static void push_r (fword* v)
+{
+    return_stack[++tos] = (uintptr_t)v;
+}
+
+inline static fword* pop_r (void)
+{
+    return (fword*)return_stack[tos--];
+}
+
 
 void* atom_next (void)
 {
@@ -27,13 +55,13 @@ void* atom_next (void)
     i_ptr++;
 
     if (tmp & 0x01) {
-        printf("Forth Word Found (%d): ", tmp);
-        // Now we would push down, but I don't have a stack implementation yet.
+        // Clear the flag before calling it.
+        tmp--;
 
-        // So just clear the bit before calling it.
-        tmp &= ~0x01;
-        
-        return (void*)tmp;
+        push_r(i_ptr);
+        i_ptr = (fword*)tmp;
+
+        return atom_next();
     } else {
         return (void*)tmp;
     }
@@ -41,9 +69,14 @@ void* atom_next (void)
 
 void* atom_literal (void)
 {
+    char numstr[20];
+
     // Interpret the next location as a number, print it and skip.
     int num = (intptr_t)*i_ptr;
-    printf("%d\n", num);
+
+    sprintf(numstr, "%d", num);
+    print_fn_msg(atom_literal, numstr);
+
     i_ptr++;
 
     return atom_next();
@@ -52,37 +85,110 @@ void* atom_literal (void)
 
 void* atom_exit (void)
 {
-    printf("EXIT\n");
-    return NULL;
+    print_fn(atom_exit);
+
+    i_ptr = pop_r();
+
+    if (i_ptr != NULL) {
+        return atom_next();
+    } else {
+        return NULL;
+    }
 }
 
-void* atom_ten (void)
+void* atom_do (void)
 {
-    printf("TEN\n");
+    void* next_instruction;
+
+    print_fn(atom_do);
+    push_r(i_ptr);
+
     return atom_next();
 }
-
-void* fib (void)
+ 
+void* atom_loop (void)
 {
-    printf("FIB\n");
+    char tmpstr[40];
+
+    fword* loop_ptr = pop_r();
+
+    sprintf(tmpstr, "to %p", *loop_ptr);
+
+    print_fn_msg(atom_loop, tmpstr);
+
+
+
     return atom_next();
 }
+ 
+
+#define CREATE_PLACEHOLDER(fn)      \
+void* fn (void)                     \
+{                                   \
+    print_fn(fn);                   \
+    return atom_next();             \
+}                                   \
+                                    \
+
+CREATE_PLACEHOLDER(atom_rot)
+CREATE_PLACEHOLDER(atom_dup)
+CREATE_PLACEHOLDER(atom_drop)
+CREATE_PLACEHOLDER(atom_plus)
+CREATE_PLACEHOLDER(atom_dot)
+
+// : fib  ( n -- )  1 1 rot 0 do dup rot dup . + loop drop drop ; 
+
+fword test[] = {
+    atom_literal,
+    FORTH_LIT(1),
+    atom_literal,
+    FORTH_LIT(2),
+    atom_literal,
+    FORTH_LIT(3),
+    atom_literal,
+    FORTH_LIT(4),
+    atom_exit
+};
 
 
+fword fib[] = {
+    atom_literal, 
+    FORTH_LIT(1),
+    atom_literal, 
+    FORTH_LIT(1),
+    atom_rot,
+    atom_literal, 
+    FORTH_LIT(0),
+    atom_do,
+    atom_dup,
+    atom_rot,
+    atom_dup,
+    FORTH_WORD(test),
+    atom_dot,
+    atom_plus,
+    atom_loop,
+    atom_drop,
+    atom_drop,
+    atom_exit
+};
+ 
 
-
+// : pgm_1         10 fib ;
 fword pgm_1[] = {
     atom_literal,
     FORTH_LIT(10),
     FORTH_WORD(fib),
     atom_exit
 };
-        
 
 
 
 int main (void)
 {
+    // Init machine
+    memset(return_stack, 0, sizeof(return_stack));
+    tos = BASE_OF_STACK;
+
     // Set to the start of the program
     i_ptr = pgm_1;
 
